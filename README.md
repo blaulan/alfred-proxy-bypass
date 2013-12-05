4e558f89613c146c6bd143f06f14ce42dda378f9
Eric <me@blaulan.com>
2013-12-05 11:55:09 +0800

    add hell mode

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 Alfred workflow to search and modify network proxy bypass list. 
 
+The workflow won't verify any domain, so be prepared for errors if you enter the wrong rule. And, you have to enter your password every time you modify network preference. 
+
 ### Search (and remove) bypass list
 
-> bypass + keyword
+> bypass KEYWORD
 
 ### Add bypass rule
 
-> bypass + rule
+> bypass RULE
 
-Yes, you have to enter your password everytime you modify network preference.
+### Batch add (no space between rules)
+
+> bypass RULE1+RULE2...
+
+### Hell mode
+
+read [Mac OS X Proxy Bypass Hell](https://w3.owind.com/pub/mac-os-x-proxy-bypass-hell/) first.
+
+> bypass DOMAIN -a
--- scripts/bypass.py
+++ scripts/bypass.py
@@ -4,7 +4,7 @@
 # @Date:   2013-12-04 15:24:56
 # @Email:  me@blaulan.com
 # @Last modified by:   Eric Wu
-# @Last Modified time: 2013-12-04 21:28:51
+# @Last Modified time: 2013-12-05 11:50:39
 
 import os
 import sys
@@ -12,14 +12,24 @@ import alfred
 import subprocess
 
 
+applescript = """
+set output to button returned of (display dialog ¬
+"%s" with title ¬
+"Are you fucking sure to add domains below to bypass?" buttons {"Yes", "No"} ¬
+default button 2 ¬
+with icon caution)
+do shell script "echo " & quoted form of output"""
+
 class bypass:
     def __init__(self):
+        self.deviceName = "Wi-Fi"
         self.bypassList = []
         self.cmdList = {
             "search": self.bypassSearch,
-            "add": self.bypassAdd,
+            "add": self.bypassAddAll,
             "rm": self.bypassRemove,
         }
+        self.bypassHellOn = False
         self.bypassRead()
 
     def bypassRead(self):
@@ -33,19 +43,35 @@ class bypass:
     def bypassAdd(self, rule):
         if rule not in self.bypassList:
             self.bypassList.append(rule)
-            return "Add '%s' to bypass list." % rule
+
+    def bypassAddAll(self, rule):
+        if self.bypassHellOn:
+            return self.bypassHell(rule)
         else:
-            return "Rule '%s' already exist." % rule
+            for item in rule.split("+"):
+                self.bypassAdd(item)
+            return "Add '%s' to list." % rule.replace("+", "', '")
+
+    def bypassHell(self, domain):
+        cmd = ["sh", "bypass.sh", domain]
+        output = subprocess.check_output(cmd)
+        confirm = "osascript -e '{}'".format(applescript % output)
+        if subprocess.check_output(confirm, shell=True).strip() == "Yes":
+            for item in output.split():
+                self.bypassAdd(item)
+            return "Add all items in '%s' to list." % domain
+        else:
+            return "Abort."
 
     def bypassRemove(self, rule):
         if rule in self.bypassList:
             self.bypassList.remove(rule)
-            return "Remove '%s' from bypass list." % rule
+            return "Remove '%s' from list." % rule
         else:
             return "Rule '%s' not exist." & rule
 
     def bypassSet(self):
-        cmd = ["networksetup", "-setproxybypassdomains", "Wi-Fi"]
+        cmd = ["networksetup", "-setproxybypassdomains", self.deviceName]
         for item in self.bypassList:
             cmd.append(item)
         subprocess.call(cmd)
@@ -53,40 +79,51 @@ class bypass:
 
     def bypassUpdate(self):
         self.bypassList = []
-        cmd = ["networksetup", "-getproxybypassdomains", "Wi-Fi"]
+        cmd = ["networksetup", "-getproxybypassdomains", self.deviceName]
         output = subprocess.check_output(cmd)
-        for item in output.split("\n"):
+        for item in output.split():
             self.bypassList.append(item)
         with open("bypass", "w") as bypassFile:
             bypassFile.write(output)
 
     def bypassSearch(self, rule):
-        inList = []
-        for item in self.bypassList:
-            if rule in item:
-                inList.append(item)
+        inList = [item for item in self.bypassList if rule in item]
         self.showResult(rule, inList)
         return "Now You See Me."
 
     def showResult(self, rule, inList):
         n = 1
-        items = []
-        if rule not in self.bypassList:
-            items.append(self.parse(0, "add", rule, "select to add rule"))
+        items = self.verifyDomain(rule)
         for item in inList:
-            items.append(self.parse(n, "rm", item, "select to remove rule"))
+            items.append(self.parse(n, "rm %s" % item, item, "REMOVE RULE"))
         alfred.write(alfred.xml(items))
+
+    def verifyDomain(self, rule):
+        subtitle = ""
+        action = "add %s" % " ".join(sys.argv[2:])
+        #if "." not in rule:
+        #    action = ""
+        #    rule = "NOT A VALID DOMAIN."
+        #    subtitle = "PLEASE EDIT YOUR INPUT"
+        if self.bypassHellOn:
+            subtitle = "ADD ALL ITEMS"
+        elif rule not in self.bypassList:
+            subtitle = "ADD RULE"
+        return ([self.parse(0, action, rule, subtitle)] if subtitle else [])
 
     def parse(self, uid, action, title, subtitle):
         return alfred.Item(
             attributes={
                 'uid': alfred.uid(uid),
-                'arg': "%s %s" % (action, title)},
-            title=title,
+                'arg': action,
+                },
+            title=title.replace("+", ", "),
             subtitle=subtitle,
             )
 
     def run(self, cmd, rule):
+        if len(sys.argv) >= 4 and sys.argv[3] == "-a":
+            self.bypassHellOn = True
         back = self.cmdList[cmd](rule)
         if cmd != "search":
             self.bypassSet()
@@ -94,4 +131,5 @@ class bypass:
 
 
 if __name__ == '__main__':
-    bypass().run(sys.argv[1], sys.argv[2])
+    if len(sys.argv) >= 3:
+        bypass().run(sys.argv[1], sys.argv[2])
